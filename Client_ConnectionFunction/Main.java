029bbaba89ff60383fe6eba75f4684a213efe75a
NetGuardian.11 <150444823+fsbfjfeuhfefehfergbgesugob@users.noreply.github.com>
2024-03-14 18:40:44 -0500

    Add files via upload

---
+++ Client_ConnectionFunction/Main.java
@@ -0,0 +1,122 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TYW_Ratter
+    public static void main(string[] args) {
+        JFrame frame = new JFrame("TYW_Ratter");
+        JButton RemoteControl = new JButton("RemoteControl");
+        JButton Keylogger = new JButton("Keylogger");
+        JButton ScreenCapture = new JButton("ScreenCapture")''
+        JButton Webcam = new JButton("Webcam");
+        JButton MicListener = new JButton("MicListener");
+        JButton FileTransfer = new JButton("FileTransfer");
+        JButton SystemInfo = new JButton("SystemInfo");
+        JButton AddToStartup = new JButton("AddToStartup");
+        JButton AntiDefender = new JButton("AntiDefender");
+        JButton NetScan = new JButton("NetScan");
+        JButton PrivilegeEscalation = new JButton("PrivilegeEscalation");
+        JButton RemoteDesktop = new JButton("RemoteDesktop");
+        JButton AddToBotnet = new JButton("AddToBotnet");
+        JButton GeoLocate = new JButton("GeoLocate");
+        JButton RSE = new JButton("RSE");
+        JButton CredentialHarvester = new JButton("CredentialHarvester");
+        JButton BrowserHijack = new JButton("BrowserHijack");
+        JButton RemoteWipe = new JButton("RemoteWipe");
+        JButton IoTControl = new JButton"("IoTControl");
+        JButton PlaySound = new JButton("PlaySound");
+        JButton RootKitInstall = new JButton("RootKitInstall");
+        JButton CrossReplicate = new JButton("CrossReplicate");
+    }
+
+    RemoteControl.AddActionListener(new ActionListener) {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            // add functions
+        }
+    }
+
+Keylogger.AddActionListener(new ActionListener) {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // add functions
+    }
+}
+
+ScreenCapture.AddActionListener(new ActionListener) {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // add functions.
+    }
+}
+
+Webcam.AddActionListener(new ActionListener) {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // add functions
+    }
+}
+
+MicListener.AddActionListener(new ActionListener) {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // add functions
+    }
+}
+
+FileTransfer.AddActionListener(new ActionListener) {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // add functions
+    }
+}
+
+SystemInfo.AddActionListener(new ActionListener) {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // add functions
+    }
+}
+
+AddToStartup.AddActionListener(new ActionListener) {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // add functions
+    }
+}
+
+AntiDefender.AddActionListener(new ActionListener) {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // add functions
+    }
+}
+
+NetScan.AddActionListener(new ActionListener) {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // add functions
+    }
+}
+
+PrivilegeEscalation.AddActionListener(new ActionListener) {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // add functions
+    }
+}
+
+RemoteDesktop.AddActionListener(new ActionListener) {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // add functions
+    }
+}
+
+AddToBotnet.AddActionListener(new ActionListener) {
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // add functions
+    }
+}
